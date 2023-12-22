4096d3e10f60fe79cc93ea854e8d97b0797f7e6d
john-wayne630 <call.me.junior.ai@gmail.com>
2023-12-22 18:33:59 +0300

    CAL-51 AI generated changes

---
+++ jest-setup.ts
@@ -0,0 +1,6 @@
+// This file can contain any global setup for jest.
+
+// Example to silence console output during tests:
+// jest.spyOn(console, 'log').mockImplementation(() => {});
+// jest.spyOn(console, 'warn').mockImplementation(() => {});
+// jest.spyOn(console, 'error').mockImplementation(() => {});
