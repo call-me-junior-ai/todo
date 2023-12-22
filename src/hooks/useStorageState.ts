522f9ef3417982fcd77929d8f0e7105a23abbe3f
call-me-junior-ai <call.me.junior.ai@gmail.com>
2023-12-22 13:32:45 +0300

    AI generated changes

--- src/hooks/useStorageState.ts
+++ src/hooks/useStorageState.ts
@@ -1,18 +1,14 @@
-import { useState, useEffect } from "react";
-/**
-A custom React hook that returns a stateful value and a function to update it, and persists the value in localStorage.
-@param {any} defaultValue - The default value for the state.
-@param {string} key - The key under which the value will be stored in localStorage.
-@returns {[any, Function]} A tuple containing the current state value and a function to update it.
-@example const [count, setCount] = useStorageState(1, "count")
-*/
-export function useStorageState<T>(
-  defaultValue: T,
-  key: string
-): [T, React.Dispatch<React.SetStateAction<T>>] {
+import { useState, useEffect } from 'react';
+
+export function useStorageState<T>(defaultValue: T, key: string): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [value, setValue] = useState<T>(() => {
-    const stickyValue = window.localStorage.getItem(key);
-    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+    const storedValue = window.localStorage.getItem(key);
+    try {
+      return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
+    } catch {
+      console.error(`Error parsing localStorage key "${key}":`, storedValue);
+      return defaultValue;
+    }
   });
 
   useEffect(() => {
