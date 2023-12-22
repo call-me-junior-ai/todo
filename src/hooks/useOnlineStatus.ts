2e74cac288cc65cc54dfb69d2be411ad5039b50d
call-me-junior-ai <call.me.junior.ai@gmail.com>
2023-12-22 13:30:14 +0300

    AI generated changes

--- src/hooks/useOnlineStatus.ts
+++ src/hooks/useOnlineStatus.ts
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
 
-/**
- * Hook that checks if a user is currently online.
- * @returns {boolean} Whether or not the user is online.
- */
-export const useOnlineStatus = (): boolean => {
-  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+export const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine);
+
+  const handleOnline = () => {
+    setOnlineStatus(true);
+  };
+
+  const handleOffline = () => {
+    setOnlineStatus(false);
+  };
 
   useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
-
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
 
     return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
     };
   }, []);
 
-  return isOnline;
+  return onlineStatus;
 };
